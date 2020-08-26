a5099fed23c9d17a02a10c1d0201634e9a9161e7
RikuKinoshita <kinoshita.run@gmail.com>
2020-08-26 18:00:01 +0900

    change design

---
+++ build/precache-manifest.3d03b9dfaa83754179df2218829e8980.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5bb39918cc66f91815348535b728f96",
+    "url": "/sushisla/build/index.html"
+  },
+  {
+    "revision": "223e12f0be10e561a7b7",
+    "url": "/sushisla/build/static/css/main.809f794b.chunk.css"
+  },
+  {
+    "revision": "6720878c5eb89bf7c93a",
+    "url": "/sushisla/build/static/js/2.af9883c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sushisla/build/static/js/2.af9883c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "223e12f0be10e561a7b7",
+    "url": "/sushisla/build/static/js/main.82179486.chunk.js"
+  },
+  {
+    "revision": "dd4a5999272879def136",
+    "url": "/sushisla/build/static/js/runtime-main.6c659da4.js"
+  }
+]);
