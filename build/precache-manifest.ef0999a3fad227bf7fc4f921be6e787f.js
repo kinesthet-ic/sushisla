7b986bfc4abf2036bb59d066926abe496c2f4d61
RikuKinoshita <kinoshita.run@gmail.com>
2020-08-26 17:11:50 +0900

    add /build

---
+++ build/precache-manifest.ef0999a3fad227bf7fc4f921be6e787f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "420c5152c38d8c99080eb905e72e2157",
+    "url": "/index.html"
+  },
+  {
+    "revision": "42885d9ad12ed0b47b68",
+    "url": "/static/css/main.809f794b.chunk.css"
+  },
+  {
+    "revision": "6720878c5eb89bf7c93a",
+    "url": "/static/js/2.af9883c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.af9883c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42885d9ad12ed0b47b68",
+    "url": "/static/js/main.c8c2c05c.chunk.js"
+  },
+  {
+    "revision": "8c8d78f2b78fe53689d0",
+    "url": "/static/js/runtime-main.9a6482af.js"
+  }
+]);
