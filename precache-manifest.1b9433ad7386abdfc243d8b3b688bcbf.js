8934f2a4873be479ee09a8f8fbe23f3be05c3f7f
RikuKinoshita <kinoshita.run@gmail.com>
2020-08-27 19:32:28 +0900

    Updates

---
+++ precache-manifest.1b9433ad7386abdfc243d8b3b688bcbf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a16626340c5aad38ab40c4008f2f2706",
+    "url": "/sushisla/build/index.html"
+  },
+  {
+    "revision": "231480dd2cb99c2b2d73",
+    "url": "/sushisla/build/static/css/main.76a295ca.chunk.css"
+  },
+  {
+    "revision": "6720878c5eb89bf7c93a",
+    "url": "/sushisla/build/static/js/2.af9883c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sushisla/build/static/js/2.af9883c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "231480dd2cb99c2b2d73",
+    "url": "/sushisla/build/static/js/main.4e1c5bbb.chunk.js"
+  },
+  {
+    "revision": "dd4a5999272879def136",
+    "url": "/sushisla/build/static/js/runtime-main.6c659da4.js"
+  }
+]);
